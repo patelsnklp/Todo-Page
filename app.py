4983a76ab241b5f8385fa96fe2397ca0ab51137f
patelsnklp <patelsnklp@gmail.com>
2025-05-23 09:46:08 +0530

    mongo uri edited app.py

--- app.py
+++ app.py
@@ -3,10 +3,9 @@ from pymongo import MongoClient
 from pymongo.server_api import ServerApi
 
 app = Flask(__name__)
-app.secret_key = "your_secret_key_here"  # For flash messages
 
 # MongoDB connection
-uri = "mongodb+srv://patelsnklp:asdfg@cluster0.6ltcang.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+uri = "mongodb+srv://<Username>:<Password>@cluster0.6ltcang.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 client = MongoClient(uri, server_api=ServerApi('1'), tlsAllowInvalidCertificates=True)
 
 # Select database and collection
